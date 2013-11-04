7889b7b263c55488d9f75b21f4fc1b14ffe5a6b7
Josh Rosen <rosenville@gmail.com>
2013-11-04 12:53:16 -0800

    Add digraph showing network's layers.

--- webui/deepviz_webui/views.py
+++ webui/deepviz_webui/views.py
@@ -1,15 +1,20 @@
-from deepviz_webui import app
+from deepviz_webui import app, cached
 
-from flask import render_template
+from flask import render_template, Response
+
+from cStringIO import StringIO
+from gpumodel import IGPUModel
+import networkx as nx
+from shownet import ShowConvNet
+import xml.etree.ElementTree as ElementTree
 
 
 _model = None
 
+
 def get_model():
     global _model
     if _model is None:
-        from shownet import ShowConvNet
-        from gpumodel import IGPUModel
         # This code is adapted from gpumodel.py and shownet.py
         load_dic = IGPUModel.load_checkpoint(app.config["TRAINED_MODEL_PATH"])
         op = ShowConvNet.get_options_parser()
@@ -18,6 +23,23 @@ def get_model():
         op = old_op
         _model = ShowConvNet(op, load_dic)
     return _model
+
+
+@app.route("/layers.svg")
+@cached()
+def layer_dag_to_svg():
+    model = get_model()
+    graph = nx.DiGraph()
+    for layer in model.layers:
+        graph.add_node(layer['name'], layer_attributes=layer)
+    for layer in model.layers:
+        for inputLayer in layer.get("inputLayers", []):
+            graph.add_edge(inputLayer['name'], layer['name'])
+    pydot_graph = nx.to_pydot(graph)
+    pydot_graph.set_rankdir("LR")
+    svg = pydot_graph.create_svg(prog="dot")
+    return Response(svg, mimetype="image/svg+xml")
+
 
 
 @app.route("/")
