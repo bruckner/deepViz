2a6357c5c07467de4d5ebcba0afdbaa3a0aef2dd
Josh Rosen <rosenville@gmail.com>
2013-11-14 22:57:16 -0800

    Use Bootstrap to layout controls and content areas

--- webui/deepviz_webui/static/js/main.js
+++ webui/deepviz_webui/static/js/main.js
@@ -6,3 +6,9 @@ function update_timeline_position(newPosition) {
 $("#timeline-slider").change(function() {
     update_timeline_position(this.value);
 });
+
+$(window).resize(function() {
+    var h = $(window).height() - $("#footer").height() - $("#header").height();
+    $('#main-contentarea-inner').height(h);
+    $('#sidebar-inner').height(h);
+}).resize();
