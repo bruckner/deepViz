474133da448278118542e0cf46c8e06303904735
Josh Rosen <rosenville@gmail.com>
2013-11-20 17:06:26 -0800

    Use tornado WSGI server.
    The Flask development server doesn't support
    concurrent requests.

--- webui/runserver.py
+++ webui/runserver.py
@@ -3,6 +3,9 @@
 import argparse
 import os
 import sys
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
 
 # Add the ConvNet scripts to the import path
 sys.path.append(os.path.join(os.path.dirname(__file__), "../scripts"))
@@ -14,4 +17,8 @@ parser.add_argument("--model", type=str, required=True)
 args = parser.parse_args()
 
 app.config["TRAINED_MODEL_PATH"] = args.model
-app.run(debug=True)
+app.debug = True
+
+http_server = HTTPServer(WSGIContainer(app))
+http_server.listen(5000)
+IOLoop.instance().start()
