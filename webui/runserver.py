1eaee5460e0286b71c6c72a50f442344326dea5a
Josh <rosenville@gmail.com>
2013-12-13 19:29:33 +0000

    Minor changes for running on EC2.

--- webui/runserver.py
+++ webui/runserver.py
@@ -15,17 +15,27 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--model", type=str, required=True)
 parser.add_argument("--cifar", type=str, required=True)
 parser.add_argument("--model-stats", type=str, required=True)
+parser.add_argument("--port", type=int, default=5000)
+parser.add_argument("--debug", action="store_true")
+parser.set_defaults(debug=False)
 args = parser.parse_args()
+
+# Needed in order to run on a server without an X GUI:
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot
+pyplot.ioff()
 
 from deepviz_webui.app import app
 
 app.config["TRAINED_MODEL_PATH"] = args.model
 app.config["CIFAR_10_PATH"] = args.cifar
 app.config["MODEL_STATS_DB"] = args.model_stats
-app.debug = True
+app.debug = args.debug
 
 http_server = HTTPServer(WSGIContainer(app))
-http_server.listen(5000)
+http_server.listen(args.port)
 ioloop = IOLoop.instance()
-autoreload.start(ioloop)
+if args.debug:
+    autoreload.start(ioloop)
 ioloop.start()
